9a469f4df748671054c0266cd27c10c3799e013a
sloanalycia <39379457+sloanalycia@users.noreply.github.com>
2019-02-04 14:33:04 -0600

    Update star_chaser.py

--- star_chaser.py
+++ star_chaser.py
@@ -4,6 +4,7 @@ import pygame
 
 from galaxy_settings import Settings
 
+from star_character import Star_Character
 
 def run_game():
 	#Initialize the game and create the screen object.
@@ -11,6 +12,9 @@ def run_game():
 	ai_settings = Settings()
 	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Star Chaser")
+	
+	#Make the character
+	nova = Star_Character(screen)
 	
 	#Set the background image
 	bg_color = (255,255,255)
@@ -31,6 +35,8 @@ def run_game():
 		screen.fill(ai_settings.bg_color)
 		
 		#screen.blit(bg_image, [0,0])
+		
+		nova.blitme()
 				
 		#Make the most recently drawn screen visible
 		pygame.display.flip()
