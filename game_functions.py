3bcef3f900729303539f9d67099e4b09a580037a
sloanalycia <39379457+sloanalycia@users.noreply.github.com>
2019-02-06 13:12:19 -0600

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -2,25 +2,36 @@ import sys
 
 import pygame
 
+
+def check_keydown_events(event,nova):
+	
+		if event.key == pygame.K_UP:
+			nova.moving_up = True
+			
+		elif event.key == pygame.K_DOWN:
+			nova.moving_down = True
+	
+def check_keyup_events(event,nova):
+	
+		if event.key == pygame.K_UP:
+			nova.moving_up = False
+					
+		elif event.key == pygame.K_DOWN:
+			nova.moving_down = False
+
 def check_events(nova):
 	#Respond to key presses and mouse events
 	for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
+		if event.type == pygame.QUIT:
+			sys.exit()
 			
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_UP:
-					nova.moving_up = True
-				
-				elif event.key == pygame.K_DOWN:
-					nova.moving_down = True
+		elif event.type == pygame.KEYDOWN:
+			check_keydown_events(event,nova)
 			
-			elif event.type == pygame.KEYUP:
-				if event.key == pygame.K_UP:
-					nova.moving_up = False
-					
-				elif event.key == pygame.K_DOWN:
-					nova.moving_down = False
+		elif event.type == pygame.KEYUP:
+			check_keyup_events(event,nova)
+			
+			
 
 def update_screen(ai_settings,screen,nova):
 	#Update images on the screen and flip to the new screen
